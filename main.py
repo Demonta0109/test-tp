fd3547c70b5c119c05056727418857060525dee7
JeanEdouart <126480369+JeanEdouart@users.noreply.github.com>
2023-05-25 00:20:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ def menu():
     print("\n==== Programming Quotes ====")
     print("1. Random quote")
     print("2. All quotes")
-    print("3. Exit")
+    print("3. Add quote")
+    print("4. Exit")
 
 
 def main():
@@ -20,6 +21,8 @@ def main():
         elif choice == "2":
             view_quotes(quotes)
         elif choice == "3":
+            add_quote(quotes,"quotes.txt")
+        elif choice == "4":
             print("Good bye...")
             break
         else:
